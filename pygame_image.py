21819664921468bd31a1744096c785a85e11370b
manato <c0b2201096@edu.teu.ac.jp>
2023-06-13 17:25:33 +0900

    練習６

--- pygame_image.py
+++ pygame_image.py
@@ -7,22 +7,32 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bbg_img = pg.transform.flip(bg_img,True,False)
     bg_img_2 = pg.image.load("ex01/fig/3.png")
     bg_img_2 = pg.transform.flip(bg_img_2,True,False)
-    kk_img = pg.transform.rotate(bg_img_2,10)
-    lis = [bg_img_2,kk_img]
-    clock = pg.time.Clock()
-    clock.tick(10)
-    tmr = 0
+    kk_img_2 = pg.transform.rotozoom(bg_img_2,1,1.0)
+    kk_img_3 = pg.transform.rotozoom(bg_img_2,3,1.0)
+    kk_img_4 = pg.transform.rotozoom(bg_img_2,5,1.0)
+    kk_img_5 = pg.transform.rotozoom(bg_img_2,8,1.0)
+    kk_img = pg.transform.rotozoom(bg_img_2,10,1.0)
+    lis = [bg_img_2,kk_img_2,kk_img_3,kk_img_4,kk_img_5,kk_img,kk_img_5,kk_img_4,kk_img_3,kk_img_2]
 
+    
+    tmr = 0
+    tmr2 = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0-tmr, 0])
-        screen.blit(lis[tmr%2], [300, 200])
+        x1 = tmr2%3200
+        screen.blit(bg_img, [-x1, 0])
+        screen.blit(bbg_img, [1600-x1, 0])
+        screen.blit(bg_img, [3200-x1, 0])
+        screen.blit(bg_img, [4800-x1, 0])
+        screen.blit(lis[tmr%10], [300, 200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1
+        tmr2 += 10
         clock.tick(10)
 
 
